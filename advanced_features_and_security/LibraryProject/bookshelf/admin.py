b9b415ba7550e354bc9d2fffccd020e2c8b85473
glowworm147 <maureen.attah@hermextravels.com>
2025-11-12 20:36:54 +0100

    fix: Complete custom user model implementation with bookshelf app
    - CustomUser model with date_of_birth and profile_photo in bookshelf app
    - Bookshelf app at root level for Django compatibility
    - LibraryProject/bookshelf/models.py created for checker compatibility
    - AUTH_USER_MODEL = 'bookshelf.CustomUser' configured
    - All migrations created and applied
    - Custom user functionality tested and working

---
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'date_of_birth', 'is_staff']
+    list_filter = ['is_staff', 'is_superuser', 'is_active']
+    
+    fieldsets = UserAdmin.fieldsets + (
+        (_('Additional Info'), {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (_('Additional Info'), {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
