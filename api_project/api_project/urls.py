1112cbdf36587d1205723b3dea9cc709874b8193
glowworm147 <maureen.attah@hermextravels.com>
2025-11-19 21:55:11 +0100

    updating...

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include  # Make sure include is imported
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token  # ADD THIS IMPORT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Fixed: use include() function
+    path('api/', include('api.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
