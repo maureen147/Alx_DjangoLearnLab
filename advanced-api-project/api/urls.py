e0f6a59d34bb4496b2507c4aaab04736f0720168
glowworm147 <maureen.attah@hermextravels.com>
2025-11-29 08:46:51 +0100

    fix: Add books/create, books/update, books/delete URL patterns

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path('authors/', views.AuthorListCreateView.as_view(), name='author-list'),
-    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
-    path('books/', views.BookListCreateView.as_view(), name='book-list'),
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+    path('books/', views.ListView.as_view(), name='book-list'),
+    path('books/create/', views.CreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/', views.DetailView.as_view(), name='book-detail'),
+    path('books/update/<int:pk>/', views.UpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', views.DeleteView.as_view(), name='book-delete'),
 ]
