5b350e41f7137a90c0cdf5b70a492e0f88031928
glowworm147 <maureen.attah@hermextravels.com>
2025-12-03 12:17:56 +0100

    fix: Add STATIC_ROOT setting

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -85,3 +85,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# Static files root for collectstatic
+STATIC_ROOT = BASE_DIR / 'staticfiles'
