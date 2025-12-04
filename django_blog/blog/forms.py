2de9a55ac0eaed75b62c83b0cd581928dba532e3
glowworm147 <maureen.attah@hermextravels.com>
2025-12-04 12:56:06 +0100

    Add TagWidget to forms.py for tag input
    - Created CustomTagWidget class inheriting from django-taggit's TagWidget
    - Updated PostForm to use TagWidget for tag input field
    - Added proper tag parsing and formatting
    - Enhanced template to display tag input with instructions
    - All form validation working correctly for tag input

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,40 +1,64 @@
 from django import forms
 from .models import Post, Comment
-from taggit.utils import parse_tags  # Import from django-taggit
+from taggit.utils import parse_tags
+from taggit.forms import TagWidget  # Import TagWidget from django-taggit
+
+# Custom TagWidget for better tag input
+class CustomTagWidget(TagWidget):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('attrs', {})
+        kwargs['attrs'].update({
+            'class': 'form-control',
+            'placeholder': 'Enter tags separated by commas (e.g., django, python, web)',
+            'data-role': 'tagsinput'
+        })
+        super().__init__(*args, **kwargs)
 
 class PostForm(forms.ModelForm):
-    tags_input = forms.CharField(
+    # Use the CustomTagWidget for tags field
+    tags = forms.CharField(
         required=False,
-        widget=forms.TextInput(attrs={
-            'placeholder': 'Enter tags separated by commas (e.g., django, python, web)',
-            'class': 'form-control'
-        }),
+        widget=CustomTagWidget(),
         help_text='Separate tags with commas. Tags will be created automatically.'
     )
     
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'tags']
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter post title'}),
-            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10, 'placeholder': 'Write your post content here...'}),
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter post title'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 10,
+                'placeholder': 'Write your post content here...'
+            }),
         }
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if self.instance.pk:
-            # For existing posts, populate tags_input with current tags
+            # For existing posts, populate tags field with current tags
             tags = self.instance.tags.all()
-            self.fields['tags_input'].initial = ', '.join([tag.name for tag in tags])
+            self.fields['tags'].initial = ', '.join([tag.name for tag in tags])
+    
+    def clean_tags(self):
+        tags_input = self.cleaned_data.get('tags', '')
+        if tags_input:
+            # Parse tags using django-taggit's utility
+            tag_names = parse_tags(tags_input)
+            return tag_names
+        return []
     
     def save(self, commit=True):
         post = super().save(commit=False)
         if commit:
             post.save()
             # Save tags using django-taggit
-            tags_input = self.cleaned_data.get('tags_input', '')
-            if tags_input:
-                tag_names = parse_tags(tags_input)
+            tag_names = self.cleaned_data.get('tags', [])
+            if tag_names:
                 post.tags.set(*tag_names)
             self.save_m2m()
         return post
